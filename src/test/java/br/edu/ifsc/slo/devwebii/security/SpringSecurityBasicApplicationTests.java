f249482245da53819ac60787e6d825e5d241e05a
“Ramao” <ramao_tiago_tiburski@hotmail.com>
2020-05-01 16:10:50 -0300

    First commit

---
+++ src/test/java/br/edu/ifsc/slo/devwebii/security/SpringSecurityBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsc.slo.devwebii.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
